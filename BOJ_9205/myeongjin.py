c536d4f859d967865d0783351a20c64b764e4701
정지수 <hzxvl@naver.com>
2023-10-23 16:26:07 +0900

    BOJ_9205 :python

--- BOJ_9205/myeongjin.py
+++ BOJ_9205/myeongjin.py
@@ -5,15 +5,15 @@ from collections import deque
 def beer(start):
     q = deque()
     q.append(start)
-    visited[start] = 1
+    visited[start] = 1 # 시작지점 방문
 
-    while q:
+    while q: # bfs로 갈 수 있는 곳을 다 찾는데,
         t = q.popleft()
 
         for i in new_arr[t]:
             if visited[i] == 0:
-                chk = min_dist(t, i)
-                if chk:
+                chk = min_dist(t, i) # 맥주거리가 1000 초과면 못 가고, 이하면 간다.
+                if chk: # 통과하면 방문
                     visited[i] = 1
                     q.append(i)
 
@@ -23,15 +23,15 @@ def beer(start):
         print('happy')
 
 
-def min_dist(f, t):
-    a, b = home_conv_fest[f]  # 시작하는 곳
-    x, y = home_conv_fest[t]
+def min_dist(f, t): # index 0 : home / index end : fest
+    a, b = home_conv_fest[f] # 시작 지점
+    x, y = home_conv_fest[t] # 끝 지점
 
-    dist = abs(x - a) + abs(y - b)
+    dist = abs(x - a) + abs(y - b) # 거리 탐색
 
-    if dist > 1000:
+    if dist > 1000: # 1000 초과 : False
         return 0
-    else:
+    else:           # 1000 이하 : True
         return 1
 
 
@@ -39,14 +39,12 @@ T = int(input())
 for tc in range(1, T + 1):
     conv_num = int(input())  # 편의점 수
     home_conv_fest = [list(map(int, input().split())) for _ in range(conv_num + 2)]
-    home = home_conv_fest[0]
-    fest = home_conv_fest[conv_num + 1]
 
-    new_arr = [[] for _ in range(conv_num + 2)]
+    new_arr = [[] for _ in range(conv_num + 2)] # 서로 다 연결되어 있다고 가정한다.
     for i in range(conv_num + 2):
         for j in range(conv_num + 2):
             if i != j:
                 new_arr[i].append(j)
 
-    visited = [0] * (conv_num + 2)
+    visited = [0] * (conv_num + 2) # 방문하는 곳
     beer(0)
