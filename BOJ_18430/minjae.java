0aa7105cd664c4d5bf2cee144ee79747d48c138b
calendar2 <zoro94317@gmail.com>
2023-10-26 18:06:16 +0900

    ♻️refactor: BOJ_18430, 설 코드 참고

--- BOJ_18430/minjae.java
+++ BOJ_18430/minjae.java
@@ -9,7 +9,10 @@ public class boj_18430 {
 	static int N;
 	static int M;
 	static int[][] tree;
+	static boolean[][] visited;
 	static int max_s = 0;
+	static int[] dr = {0, 1, 0, -1};
+    static int[] dc = {-1, 0, 1, 0};
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,7 +23,7 @@ public class boj_18430 {
 		
 		// 나무 재료 입력 받기
 		tree = new int[N][M];
-		boolean[][] visited = new boolean[N][M];
+		visited = new boolean[N][M];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
@@ -28,88 +31,52 @@ public class boj_18430 {
 			}
 		}
 		
-		backtracking(0, visited, 0);
+		backtracking(0, 0);
 		System.out.println(max_s);
+//		for (int i = 0; i < N; i++) {
+//			for (int j = 0; j < M; j++) {
+//				System.out.println(tree[i][j]);
+//			}
+//		}
 	}
 
-	static void backtracking(int s, boolean[][] visit, int cnt) {
-		if(cnt == 2) {
-			if(max_s < s) {
-				max_s = s;
-			}
-			return;
-		}
-		
-		// 배열 복사
-		boolean[][] v = new boolean[N][M];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				v[i][j] = visit[i][j];
-			}
-		}
-		
-		int r = -1;
-		int c = -1;
+	static void backtracking(int idx, int cnt) {
+//		if(cnt == 2) {
+//			if(max_s < s) {
+//				max_s = s;
+//			}
+//			return;
+//		}
 		
 		// 첫 번째 값 찾기
-		int strength1 = 0;
-		for (int i = 0; i < N; i++) {
+		for (int i = idx; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if(!v[i][j]) {
-					r = i;
-					c = j;
-					
-					// ㄴ자부터 시계방향으로 4번
+				if(!visited[i][j]) {
+
+					// ㄱ자부터 시계방향으로 4번
 					for (int k = 0; k < 4; k++) {
-						int temp1 = 0;
-						switch(k) {
-							case 0:
-								if((r-1) >= 0 && (c+1) < M) {
-									temp1 = (tree[r][c]*2) + tree[r-1][c] + tree[r][c+1];
-								}
-								break;
-							case 1:
-								if((r+1) < N && (c+1) < M) {
-									temp1 = (tree[r][c]*2) + tree[r+1][c] + tree[r][c+1];
-								}
-								break;
-							case 2:
-								if((r+1) < N && (c-1) >= 0) {
-									temp1 = (tree[r][c]*2) + tree[r+1][c] + tree[r][c-1];
-								}
-								break;
-							case 3:
-								if((r-1) >= 0 && (c-1) >= 0) {
-									temp1 = (tree[r][c]*2) + tree[r-1][c] + tree[r][c-1];
-								}
-								break;
-						}
-						if(strength1 < temp1) {
-							strength1 = temp1;
-							v[r][c] = true;
-							switch(k) {
-								case 0:
-									v[r-1][c] = true;
-									v[r][c+1] = true;
-									break;
-								case 1:
-									v[r+1][c] = true;
-									v[r][c+1] = true;
-									break;
-								case 2:
-									v[r+1][c] = true;
-									v[r][c-1] = true;
-									break;
-								case 3:
-									v[r-1][c] = true;
-									v[r][c-1] = true;
-									break;
+						int nr1 = i + dr[k];
+						int nc1 = j + dc[k];
+						
+						int nr2 = i + dr[(k+1)%4];
+						int nc2 = j + dc[(k+1)%4];
+						
+						if(nr1 >= 0 && nr1 < N && nc1 >= 0 && nc1 < M
+								&& nr2 >= 0 && nr2 < N && nc2 >= 0 && nc2 < M) {
+							if(!visited[nr1][nc1] && !visited[nr2][nc2]) {
+								visited[i][j] = true;
+								visited[nr1][nc1] = true;
+								visited[nr2][nc2] = true;
+								backtracking(i, cnt + 2*tree[i][j] + tree[nr1][nc1] + tree[nr2][nc2]);
+								visited[i][j] = false;
+								visited[nr1][nc1] = false;
+								visited[nr2][nc2] = false;
 							}
 						}
 					}
-					backtracking(s + strength1, v, cnt+1);
 				}
 			}
 		}
+		max_s = Math.max(max_s, cnt);
 	}
 }
