65adcecbfd754c5e789c4b10c304ba413eff743d
sirlyun <sirlyun23@gmail.com>
2023-10-26 14:48:46 +0900

    ✨error: BOJ_18430 오답 코드(시간초과 2차;;;)

--- BOJ_18430/sirlyun.java
+++ BOJ_18430/sirlyun.java
@@ -42,7 +42,7 @@ public class Main {
             }
         }
 
-        dfs(0);
+        dfs(0, 0);
 
         bw.write(Integer.toString(result));
 
@@ -52,60 +52,50 @@ public class Main {
 
     }
 
-    static void dfs(int cnt){
+    static void dfs(int idx, int cnt){
 
-        // 중심 정하기
-        for (int i=0; i<N; i++){
-            for (int j=0; j<M; j++){
-                // 중심이 아직 선택되지 않았을 때
-                if (!visited[i][j]){
-                    // 일단 중심 방문한 것으로 체크
-                    visited[i][j] = true;
-                    // 현재를 중심으로 했을 때 가능한 부메랑 모양 체크
-                    for (int k=0; k<4; k++){
-                        boolean flag = true;
-                        List<Integer[]> chk = new ArrayList<>();
-                        for (int l=0; l<2; l++){
-                            int di = i + possible[k][l][0];
-                            int dj = j + possible[k][l][1];
-                            // 인덱스 범위를 넘어가면 현재 부메랑 모양으로 만들 수 없다
-                            if (0>di || di>=N || 0>dj || dj>=M){
-                                flag = false;
-                                break;
-                            }
-                            else{
-                                chk.add(new Integer[]{di, dj});
-                                // 이미 방문한 곳이 현재 부메랑 모양에 포함되면 만들 수 없다
-                                if (visited[di][dj]){
-                                    flag = false;
-                                    break;
-                                }
-                            }
-                        }
-                        // 현재 부메랑 모양으로 만들 수 있을 때
-                        if (flag){
-                            // 부메랑 모양에 포함되는 곳들을 모두 방문 처리
-                            visited[chk.get(0)[0]][chk.get(0)[1]] = true;
-                            visited[chk.get(1)[0]][chk.get(1)[1]] = true;
-                            // 계산해 가며 재귀
-                            dfs(cnt + 2*board[i][j] + board[chk.get(0)[0]][chk.get(0)[1]] + board[chk.get(1)[0]][chk.get(1)[1]]);
-                            visited[chk.get(0)[0]][chk.get(0)[1]] = false;
-                            visited[chk.get(1)[0]][chk.get(1)[1]] = false;
-                        }
+        if(idx == N*M){
+            result = Math.max(result, cnt);
+            return;
+        }
+
+        int i = idx/M;
+        int j = idx%M;
+
+        // 중심이 아직 선택되지 않았을 때
+        if (!visited[i][j]){
+            // 현재를 중심으로 했을 때 가능한 부메랑 모양 체크
+            for (int k=0; k<4; k++){
+                int di1 = i + possible[k][0][0];
+                int dj1 = j + possible[k][0][1];
+
+                int di2 = i + possible[k][1][0];
+                int dj2 = j + possible[k][1][1];
+
+                if (0<=di1 && 0<=di2 && 0<=dj1 && 0<=dj2 && di1<N && di2<N && dj1<M && dj2<M){
+                    if (!visited[di1][dj1] && !visited[di2][dj2]){
+                        visited[i][j] = true;
+                        visited[di1][dj1] = true;
+                        visited[di2][dj2] = true;
+                        // 계산해 가며 재귀
+                        dfs(idx+1, cnt + 2*board[i][j] + board[di1][dj1] + board[di2][dj2]);
+                        visited[i][j] = false;
+                        visited[di1][dj1] = false;
+                        visited[di2][dj2] = false;
                     }
-
-                    visited[i][j] = false;
+                    else {
+                        dfs(idx+1, cnt);
+                    }
                 }
-
+                else {
+                    dfs(idx+1, cnt);
+                }
             }
-            // 최댓값 갱신
-            if (i == N-1){
-                result = Math.max(result, cnt);
-            }
+        }
+        else {
+            dfs(idx+1, cnt);
         }
 
     }
-
-
 
 }
