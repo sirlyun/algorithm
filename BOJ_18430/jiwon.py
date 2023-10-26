e70affbe923658644802d91af50df31815ea52c1
jw <jiwoni1210@naver.com>
2023-10-26 11:57:21 +0900

    ♻️refactor : BOJ_18430 블로그 참고

--- BOJ_18430/jiwon.py
+++ BOJ_18430/jiwon.py
@@ -1,31 +1,25 @@
 n, m = map(int,input().split())
 arr = [list(map(int,input().split())) for _ in range(n)]
-bum = [[(0,-1),(1,0)],[(0,-1),(-1,0)],[(-1,0),(0,1)],[(0,1),(1,0)]]
-
-max_strength = 0
+bum = [(0,-1,1,0),(-1,0,0,-1),(-1,0,0,1),(0,1,1,0)] # 부메랑 4가지 모양
 visited = [[0] * m for _ in range(n)]
-
+ans = 0
 def dfs(i,j,strength):
-    global max_strength
-    max_strength = max(strength, max_strength)
-    if visited[i][j] == 1:
+    global ans
+    if j == m:  # 행을 다 돌았을 때 다음 열로 고.
+        i += 1
+        j = 0
+    if i == n:
+        ans = max(ans,strength) # 최댓값 갱신
         return
-    else:
-        for k in range(4):
-            flag = 0
-            for h in range(2):
-                if 0 <= i + bum[k][h][0] < n and 0 <= j + bum[k][h][1] < m:
-                    if visited[i + bum[k][h][0]][j + bum[k][h][1]] == 1:
-                        flag = -1
-            if flag == 0:
-                    # 백트래킹...?
-                    visited[i + bum[k][0][0]][j + bum[k][0][1]] = 1
-                    visited[i + bum[k][1][0]][j + bum[k][1][1]] = 1
-                    dfs(i,j,strength + arr[i][j]*2 + arr[i + bum[k][0][0]][j + bum[k][0][1]]+arr[i + bum[k][1][0]][j + bum[k][1][1]])
-                    visited[i + bum[k][0][0]][j + bum[k][0][1]] = 0
-                    visited[i + bum[k][1][0]][j + bum[k][1][1]] = 0
+    if visited[i][j] == 0:
+        for k in range(4): # 4가지 모양 가능한지 모두 탐색
+            a, b, c, d = bum[k]
+            x1,y1,x2,y2 = i+a, j+b, i+c, j+d
+            if 0<= x1 <n and 0<= y1 <m and 0<= x2 <n and 0<= y2 <m and visited[x1][y1] == 0 and visited[x2][y2] == 0:
+                visited[x1][y1] = visited[x2][y2] = visited[i][j] = 1
+                dfs(i,j+1,strength + arr[i][j]*2 + arr[x1][y1] + arr[x2][y2]) # 백트래킹
+                visited[x1][y1] = visited[x2][y2] = visited[i][j] = 0
+    dfs(i,j+1,strength)
 
-# 중심위치 정하기(i,j)
-for i in range(n):
-    for j in range(m):
-        dfs(i,j,0)
+dfs(0,0,0)
+print(ans)
