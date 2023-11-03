989f38ab33b24f5b82289a90e3f9038a50c862ed
jw <jiwoni1210@naver.com>
2023-11-03 13:59:24 +0900

    ♻️refactor :  BOJ_1939

--- BOJ_1939/jiwon.py
+++ BOJ_1939/jiwon.py
@@ -1,26 +1,45 @@
-n, m = map(int,input().split())
-arr = [[] for _ in range(n+1)]
+import sys
+
+input = sys.stdin.readline
+
+def check(weight): # 목적지 도착 여부를 체크하는 함수
+    visited = [False] * (n + 1)
+    visited[start] = True
+    stack = [start]
+
+    while stack:
+
+        current = stack.pop()
+
+        if current == end: # 목적지 도착!
+            return True
+
+        for node, bridge_w in arr[current]:
+            if bridge_w >= weight and not visited[node]: # 들고온 짐 그대로 건너갈 수 있나?
+                visited[node] = True
+                stack.append(node)
+
+    return False
+
+n, m = map(int, input().split())
+arr = [[] for _ in range(n + 1)]
 for _ in range(m):
-    a, b, c = map(int,input().split()) # a번섬과 b번섬 사이에 중량제한 c인 (양방향)다리 존재
-    arr[a].append((b,c))
-    arr[b].append((a,c))
+    a, b, c = map(int, input().split())
+    arr[a].append((b, c)) # 양방향이므로 둘 다 넣어줌!
+    arr[b].append((a, c))
 
-start, end = map(int,input().split()) # 공장이 있는 두 섬의 번호
-# 공장->공장 이동할 때 다리의 최댓값?
+start, end = map(int, input().split())
 
-visited = [0]*(n+1)
-ans = 0
+# 이진탐색 이용
+left = 1
+right = 1000000000
 
-def dfs(node,cost):
-    global ans
-    if node == end:
-        ans = max(ans, cost)
-        return
-    visited[node] = 1
-    for i, j in arr[node]:
-        if not visited[i]:
-            dfs(i, j)
-    visited[node] = 0
-    return
-dfs(start,0)
-print(ans)
+while left <= right:
+    mid = (left + right) // 2
+
+    if check(mid): # 시작지점에서 mid의 중량이 끝까지 도착할 수 있으면?
+        left = mid + 1
+    else:
+        right = mid - 1
+
+print(right)
