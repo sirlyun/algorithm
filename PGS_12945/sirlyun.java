c61c249fe2581d606ce614d77960c2b1a002676d
sirlyun <sirlyun23@gmail.com>
2024-01-03 20:14:44 +0900

    PGS_12945: Java

---
+++ PGS_12945/sirlyun.java
@@ -0,0 +1,22 @@
+class Solution {
+    
+    public int fibo(int n){
+        
+        int[] fibo = new int[n+1];
+        fibo[0] = 0;
+        fibo[1] = 1;
+        
+        for (int i=2; i<=n; i++){
+            fibo[i] = (fibo[i-1] + fibo[i-2]) % 1234567;
+        }
+        
+        return fibo[n];
+    }
+    
+    public int solution(int n) {
+        
+        int answer = fibo(n);
+        
+        return answer % 1234567;
+    }
+}
