908d91e2e4bd1091c9e00de7445b6c091a8ddacc
sirlyun <116432941+sirlyun@users.noreply.github.com>
2024-04-15 17:13:32 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 210.24 ms, Memory: 23.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
@@ -0,0 +1,18 @@
+'''
+    f(x) = x보다 크고 x와 비트가 1~2개 다른 수들 중 제일 작은 수
+'''
+
+def solution(numbers):
+    answer = []
+
+    for number in numbers:
+        bin_number = list('0' + bin(number)[2:])
+        idx = ''.join(bin_number).rfind('0')
+        bin_number[idx] = '1'
+        
+        if number % 2 == 1:
+            bin_number[idx+1] = '0'
+            
+        answer.append(int(''.join(bin_number), 2))
+
+    return answer
